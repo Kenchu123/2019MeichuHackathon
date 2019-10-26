3c49976f49b44f13253d647d782c4b086bfdc902
Kenchu123 <k88913n@gmail.com>
2019-10-26 21:27:06 +0800

    upload pdf

--- ui/src/client/component/PDFManager.js
+++ ui/src/client/component/PDFManager.js
@@ -10,40 +10,51 @@ class PDFManager extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            isRead: false,
-            isUploading: false
+            uploadStatus: '',
         }
         this.uploadPDF = this.uploadPDF.bind(this)
         this.pdf = null
     }
-    uploadPDF() {
-        console.log("uploading pdf")
-        let url = '/ds093.pdf'
-        this.setState({ isUploading: true })
-        pdfjsLib.getDocument(url).promise.then(pdf => {
-            this.pdf = pdf
-            this.setState({
-                isRead: true,
-                isUploading: false
+    uploadPDF(e) {
+        e.preventDefault()
+        let file = e.target.files[0]
+        if (file.type !== "application/pdf") {
+            console.error(file.name, 'is not a pdf file.')
+            return
+        }
+        // open uploaded file
+        let fileReader = new FileReader()
+        fileReader.onload = (e) => {
+            this.setState({ uploadStatus: 'Loading...' })
+            
+            // load pdf with pdf js
+            let typedarray = new Uint8Array(e.target.result)
+            pdfjsLib.getDocument(typedarray).promise.then(pdf => {
+                // upload finish
+                this.pdf = pdf
+                this.setState({ uploadStatus: '' })
+            }, (err) => {
+                this.setState({ uploadStatus: 'Fail to upload!!'})
+                console.error(err)
             })
-            console.log("upload finish!")
-        }, (err) => {
-            console.error(err)
-        })
+        }
+        fileReader.readAsArrayBuffer(file)
     }
     render() {
         let pdfViewer
-        if (this.state.isUploading) {
-            pdfViewer = <p>Loading...</p>
+        if (this.state.uploadStatus !== '') {
+            pdfViewer = <p>{this.state.uploadStatus}</p>
         }
-        else if (this.state.isRead && !this.state.isUploading) {
+        else if (this.pdf){
             pdfViewer = <PDFViewer pdf={this.pdf} />
         }
 
         return (
             <div>
-                <p>This is for pdf</p>
-                <button onClick={this.uploadPDF}>Click to upload pdf</button>
+                <h3>Upload datasheet and try to get your microchip!</h3>
+                <p></p>
+                <input type='file' accept="application/pdf" onChange={e => this.uploadPDF(e)}></input>
+                <br />
                 {pdfViewer}
             </div>
         )
