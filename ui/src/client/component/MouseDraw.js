f5cffbd64e124188c72cfcd457bb10192f15c47c
Kenchu123 <k88913n@gmail.com>
2019-10-27 02:28:39 +0800

    two canvs

--- ui/src/client/component/MouseDraw.js
+++ ui/src/client/component/MouseDraw.js
@@ -1,38 +1,64 @@
 import React from 'react'
 
-class MouseDraw {
-    constructor() {
-        this.last_mouseX = 0
-        this.last_mouseY = 0
-        this.mouseIsDown = false
+class MouseDraw extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            last_mouseY: 0,
+            last_mouseX: 0,
+            mouseIsDown: false,
+        }
+        this.drawRef = React.createRef()
+        this.mouseDown = this.mouseDown.bind(this)
+        this.mouseUp = this.mouseUp.bind(this)
+        this.mouseMove = this.mouseMove.bind(this)
     }
 
-    mouseDown(e, canvas) {
+    componentDidMount() {
+        console.log(this.props)
+    }
+
+    mouseDown(e) {
         console.log('mouseDown')
+        let canvas = this.drawRef.current
         const canPos = canvas.getBoundingClientRect()
-        this.last_mouseX = parseInt(e.clientX - canPos.left)
-        this.last_mouseY = parseInt(e.clientY - canPos.top)
-        this.mouseIsDown = true
+        this.state.last_mouseX = parseInt(e.clientX - canPos.left)
+        this.state.last_mouseY = parseInt(e.clientY - canPos.top)
+        this.state.mouseIsDown = true
     }
 
-    mouseUp(e, canvas) {
+    mouseUp(e) {
         console.log('mouseUp')
-        this.mouseIsDown = false
+        this.state.mouseIsDown = false
     }
 
-    mouseMove(e, canvas) {
+    mouseMove(e) {
+        let canvas = this.drawRef.current
         const ctx = canvas.getContext('2d')
-
-        if (this.mouseIsDown) {
+        console.log(canvas)
+        if (this.state.mouseIsDown) {
             ctx.clearRect(0, 0, canvas.width, canvas.height)
             ctx.beginPath()
-            let width = e.clientX - this.last_mouseX - canvas.getBoundingClientRect().left;
-            let height = e.clientY - this.last_mouseY - canvas.getBoundingClientRect().top;
-            ctx.rect(this.last_mouseX, this.last_mouseY, width, height);
-            ctx.strokeStyle = 'black';
-            ctx.lineWidth = 10;
-            ctx.stroke();
+            let width = e.clientX - this.state.last_mouseX - canvas.getBoundingClientRect().left
+            let height = e.clientY - this.state.last_mouseY - canvas.getBoundingClientRect().top
+            ctx.rect(this.state.last_mouseX, this.state.last_mouseY, width, height)
+            ctx.strokeStyle = 'black'
+            ctx.lineWidth = 3
+            ctx.stroke()
         }
+    }
+    render() {
+        return (
+            <canvas
+                ref={this.drawRef}
+                width={this.props.width}
+                height={this.props.height}
+                style={{border: '1px solid'}}
+                onMouseDown={e => this.mouseDown(e)}
+                onMouseMove={e => this.mouseMove(e)}
+                onMouseUp={e => this.mouseUp(e)}
+            />
+        )
     }
 }
 
