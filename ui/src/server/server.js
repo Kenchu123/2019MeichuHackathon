8941b97d1a5550ef5439f9ea3379a99de2c87210
Kenchu123 <k88913n@gmail.com>
2019-10-27 06:36:17 +0800

    rebase

--- ui/src/server/server.js
+++ ui/src/server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
-
 const webpackConfig = require('../../webpack.dev.js');
+
+const spawn = require('child_process').spawn
+const multer = require('multer')
+const upload = multer({ dest: '../../uploads/'})
+
+const fs = require('fs')
 
 // Setup an Express server
 const app = express();
@@ -17,7 +22,32 @@ if (process.env.NODE_ENV === 'development') {
   app.use(express.static('dist'));
 }
 
+// api
+// download pdf, store in ui/uploads
+app.post('/api/download', upload.single('pdf'), (req, res, next) => {
+  let file = req.file
+  if (!file) {
+    const err = new Error('Please upload a file')
+    err.httpStatusCode = 400
+    return next(err)
+  }
+  fs.readFile(file.path, (err, data) => {
+    fs.writeFile(`../uploads/${file.originalname}`, data, (err) => {
+      if (err) console.log(err)
+    })
+  })
+
+  res.send({success: true})
+})
+// for get user input (which is pdf place)
+
 // Listen on port
 const port = process.env.PORT || 3000;
 const server = app.listen(port);
 console.log(`Server listening on port ${port}`);
+
+// python process
+// const pythonProcess = spawn('python3', ['./test.py'])
+// pythonProcess.stdout.on('data', data => {
+//   console.log(data.toString())
+// })
