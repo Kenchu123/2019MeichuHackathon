1ea8fcbe941fdf7611ce3184d39a4670ee7fc297
Kenchu123 <k88913n@gmail.com>
2019-10-26 18:04:26 +0800

    upload pdf

---
+++ ui/src/client/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './component/App.js'
+import './css/index.css'
+
+ReactDOM.render(<App />, document.getElementById('app'))
