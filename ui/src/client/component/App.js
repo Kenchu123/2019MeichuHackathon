a72c7455e97af83b2ba54957aafdd0cad95abbf7
tzyLee <b06901145@ntu.edu.tw>
2019-10-27 08:59:01 +0800

    Implement MicroChip

--- ui/src/client/component/App.js
+++ ui/src/client/component/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PDFManager from './PDFManager';
 import ResponsiveDrawer from './ResponsiveDrawer';
+import MicroChip from './MicroChip';
 
 const App = () => (
   <div className="App">
     <ResponsiveDrawer>
+      <MicroChip width="1224" height="1584" />
       <PDFManager />
     </ResponsiveDrawer>
   </div>
