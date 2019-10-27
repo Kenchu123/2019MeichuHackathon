cd73e30a937e59c00346054c36efefb6f5f37b90
Kenchu123 <k88913n@gmail.com>
2019-10-27 11:48:29 +0800

    imgCapture

--- parser/capture_pic.py
+++ parser/capture_pic.py
@@ -1,6 +1,8 @@
 from pdf2image import convert_from_path
+import sys
 
-def ImageCapture(path, page, size, pos, out_name):
+def ImageCapture(path, page, size, pos, out_path):
+    print("Start Capture Image")
     images = convert_from_path(path, dpi=300, first_page=page, single_file=True, fmt='png', size = size)
     image = images[0]
     width, height = image.size
@@ -8,13 +10,20 @@ def ImageCapture(path, page, size, pos, out_name):
     y1 = height - y1
     y2 = height - y2
     cropImg = image.crop((x1, y2, x2, y1))
-    cropImg.save(out_name)
+    cropImg.save(out_path)
+    print("Image Capture Finish!")
     return cropImg
 
 if __name__ == '__main__':
-    width = 1224
-    height = 1584
-    x1, y1, x2, y2 = 0, 658, 1224, 1480 # left lower to right upper
+    # name = 'ds093'
+    # page = 17
+    width = 612
+    height = 792
+    # x1, y1, x2, y2 = 0, 658, 1224, 1480 # left lower to right upper
+    name, page, x1, y1, x2, y2 = sys.argv[1], int(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6])
+    print(sys.argv)
     # return Croped Img
-    cropImg = ImageCapture('../data/ds093.pdf', 17, (width, height), (x1, y1, x2, y2), 'test.png')
-    cropImg.show()
+    path = ''.join(('../uploads/', name, '.pdf'))
+    out_path = ''.join(('../pictures/', name, '.png'))
+    cropImg = ImageCapture(path, page, (width, height), (x1, y2, x2, y1), out_path)
+    # cropImg.show()
---
+++ parser/test.py
@@ -0,0 +1,9 @@
+import sys
+name = sys.argv[1]
+page = sys.argv[2]
+x1 = sys.argv[3]
+y1 = sys.argv[4]
+x2 = sys.argv[5]
+y2 = sys.argv[6]
+# name, page, x1, y1, x2, y2 = sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7]
+print(sys.argv)
